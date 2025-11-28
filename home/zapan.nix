2b3da3025e4eaa6ad1a25ea50034ccfe60e3c8c4
Zapan <g0ne150@hotmail.com>
2025-11-29 00:27:09 +0800

    updating git config

--- home/zapan.nix
+++ home/zapan.nix
@@ -6,10 +6,8 @@
 
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Zapan Gao";
-      email = "g0ne150@hotmail.com";
-    };
+    userName = "Zapan Gao";
+    userEmail = "g0ne150@hotmail.com";
   };
   programs.bash = { enable = true; };
 
