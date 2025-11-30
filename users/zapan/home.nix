be51d873022b580fe70c82ec2c089d4b0ee21a65
Zapan Gao <g0ne150@hotmail.com>
2025-11-30 16:44:09 +0800

    adding polkit gnome agent systemd user service

--- users/zapan/home.nix
+++ users/zapan/home.nix
@@ -43,5 +43,23 @@ in
 
   dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
+systemd.user.services.polkit-gnome-authentication-agent-1 = {
+  Unit = {
+    Description = "polkit-gnome-authentication-agent-1";
+    Wants = [ "graphical-session.target" ];
+    After = [ "graphical-session.target" ];
+  };
+  Install = {
+    WantedBy = [ "graphical-session.target" ];
+  };
+  Service = {
+    Type = "simple";
+    ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    Restart = "on-failure";
+    RestartSec = 1;
+    TimeoutStopSec = 10;
+  };
+};
+
   home.stateVersion = "25.05";
 }
