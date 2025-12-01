178409fc0ce3f475495e0bafb334b992beb19048
Zapan Gao <g0ne150@hotmail.com>
2025-12-01 15:18:02 +0800

    fix tlp error

--- modules/battery.nix
+++ modules/battery.nix
@@ -3,7 +3,9 @@
 
   services.tlp = {
     enable = true;
-    START_CHARGE_THRESH_BAT0 = 75; # 75 and below it starts to charge
-    STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 75; # 75 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+    };
   };
 }
