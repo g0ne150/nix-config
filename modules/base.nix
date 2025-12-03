7b7d36a2fd4ffbf2cc7e3ad4982025ef9a8a7199
Zapan Gao <g0ne150@hotmail.com>
2025-12-03 22:14:23 +0800

    add pkg nix-tree

--- modules/base.nix
+++ modules/base.nix
@@ -50,6 +50,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     home-manager
+    nix-tree
     wget
     git
     curl
