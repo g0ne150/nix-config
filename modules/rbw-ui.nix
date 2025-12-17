6327c0bd1ccdb83e35dd67757d792601e43278cf
Zapan Gao <g0ne150@hotmail.com>
2025-12-17 11:09:43 +0800

    update flake lock; fix typo

--- modules/rbw-ui.nix
+++ modules/rbw-ui.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [ dotool rofi-rbw-wayland ];
   programs.rbw = {
     enable = true;
-    settigns = {
+    settings = {
       base_url = "https://vaultwarden.zapan.club:10";
       email = "g0ne150@hotmail.com";
     };
