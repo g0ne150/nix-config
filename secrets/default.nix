424ecf67f106a20823188c5fd5153e56ca20b173
Zapan Gao <g0ne150@hotmail.com>
2026-01-17 12:13:35 +0800

    use agenix manages ssh keys

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,7 +1,36 @@
-{ ... }: {
+{ ... }:
+{
   age = {
     identityPaths = [ "/var/lib/agenix/zenbook" ];
     secrets = {
+      ssh_key_general = {
+        file = ./ssh_key_general.age;
+        path = "/home/zapan/.ssh/general";
+        mode = "400";
+        owner = "zapan";
+        group = "users";
+      };
+      ssh_key_bandwagon = {
+        file = ./ssh_key_bandwagon.age;
+        path = "/home/zapan/.ssh/bandwagon";
+        mode = "400";
+        owner = "zapan";
+        group = "users";
+      };
+      ssh_key_home_git = {
+        file = ./ssh_key_home_git.age;
+        path = "/home/zapan/.ssh/home_git";
+        mode = "400";
+        owner = "zapan";
+        group = "users";
+      };
+      ssh_key_github_rsa = {
+        file = ./ssh_key_github_rsa.age;
+        path = "/home/zapan/.ssh/github_rsa";
+        mode = "400";
+        owner = "zapan";
+        group = "users";
+      };
       deepseek_api_key = {
         file = ./deepseek_api_key.age;
         group = "users";
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,18 @@
 let
-  zapan =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy7D9IFOBh22J6dPDq/XURswrpCrmnlsdWHefjGUtPr zapan@zenbook";
-  zenbook =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZBKgTiWIU358jlDvPB5qPf2i5szeVnLLGJrOy0WN0W root@zenbook";
+  zapan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy7D9IFOBh22J6dPDq/XURswrpCrmnlsdWHefjGUtPr zapan@zenbook";
+  zenbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZBKgTiWIU358jlDvPB5qPf2i5szeVnLLGJrOy0WN0W root@zenbook";
 
-  default = [ zapan zenbook ];
-in {
+  default = [
+    zapan
+    zenbook
+  ];
+in
+{
+  "agenix_zapan_key.age".publicKeys = default; # agenix zapan private key
+  "ssh_key_general.age".publicKeys = default;
+  "ssh_key_bandwagon.age".publicKeys = default;
+  "ssh_key_home_git.age".publicKeys = default;
+  "ssh_key_github_rsa.age".publicKeys = default;
   "deepseek_api_key.age".publicKeys = default;
   "context7_api_key.age".publicKeys = default;
   "moonshot_api_key.age".publicKeys = default;
