08800c18cc7a726d8f85399a467717f705b92ed3
Zapan Gao <g0ne150@hotmail.com>
2026-01-26 10:49:02 +0800

    update flake; remove mcp

--- modules/claude-code.nix
+++ modules/claude-code.nix
@@ -3,19 +3,19 @@ let
   common_script = ''
     #!/bin/sh
     set -eu
-    export CONTEXT7_API_KEY="$(cat ${ageSecrets.context7_api_key.path})"
+    # export CONTEXT7_API_KEY="$(cat ${ageSecrets.context7_api_key.path})"
 
   '';
 in {
   programs.claude-code = {
     enable = true;
-    mcpServers = {
-      context7 = {
-        type = "http";
-        url = "https://mcp.context7.com/mcp";
-        headers = { CONTEXT7_API_KEY = "\${CONTEXT7_API_KEY}"; };
-      };
-    };
+    # mcpServers = {
+    #   context7 = {
+    #     type = "http";
+    #     url = "https://mcp.context7.com/mcp";
+    #     headers = { CONTEXT7_API_KEY = "\${CONTEXT7_API_KEY}"; };
+    #   };
+    # };
   };
 
   home.file.".local/bin/claude-deepseek" = {
