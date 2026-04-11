658358a8d9013f19eec539137baaf923f99c6253
Zapan Gao <g0ne150@hotmail.com>
2026-04-11 21:52:29 +0800

    add spring-boot.nvim plugin for neovim

--- modules/lazyvim/jdtls.nix
+++ modules/lazyvim/jdtls.nix
@@ -1,5 +1,6 @@
 { pkgs, lib, ... }:
 let
+  vscode-extension-path = "share/vscode/extensions";
   vscode-spring-boot = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     # https://open-vsx.org/api/VMware/vscode-spring-boot/2.2.2026040900/file/VMware.vscode-spring-boot-2.2.2026040900.vsix
     mktplcRef = {
@@ -10,6 +11,8 @@ let
       # sha256 = lib.fakeSha256;
     };
   };
+  vscode-spring-boot-path = "${vscode-spring-boot}/${vscode-extension-path}/VMware.vscode-spring-boot";
+  vscode-spring-boot-path-jars = "${vscode-spring-boot-path}/jars";
 in
 {
   programs.lazyvim = {
@@ -29,15 +32,19 @@ in
     plugins.jdtls = ''
       local bundles = {}
       local jar_patterns = {
-        "${pkgs.vscode-extensions.vscjava.vscode-java-dependency}/share/vscode/extensions/vscjava.vscode-java-dependency/server/com.microsoft.jdtls.ext.core-*.jar",
-        "${vscode-spring-boot}/share/vscode/extensions/VMware.vscode-spring-boot/jars/*.jar",
+        "${pkgs.vscode-extensions.vscjava.vscode-java-dependency}/${vscode-extension-path}/vscjava.vscode-java-dependency/server/com.microsoft.jdtls.ext.core-*.jar",
+        "${vscode-spring-boot-path-jars}/io.projectreactor.reactor-core.jar",
+        "${vscode-spring-boot-path-jars}/org.reactivestreams.reactive-streams.jar",
+        "${vscode-spring-boot-path-jars}/jdt-ls-commons.jar",
+        "${vscode-spring-boot-path-jars}/jdt-ls-extension.jar",
+        "${vscode-spring-boot-path-jars}/sts-gradle-tooling.jar",
       }
       for _, jar_pattern in ipairs(jar_patterns) do
         for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), "\n")) do
           table.insert(bundles, bundle)
         end
       end
-      vim.notify(vim.json.encode(bundles))
+
       return {
         "mfussenegger/nvim-jdtls",
         opts = function(_, opts)
@@ -59,24 +66,23 @@ in
         end,
       }
     '';
-  };
 
-  # plugins.java-deps = ''
-  #   return {
-  #     {
-  #       "g0ne150/java-deps.nvim",
-  #       -- dir = "~/myspace/neovim/java-deps.nvim/",
-  #       dependencies = {
-  #         "folke/snacks.nvim",
-  #         # {
-  #         #   "mason-org/mason.nvim",
-  #         #   opts = { ensure_installed = { "vscode-java-dependency" } },
-  #         # },
-  #       },
-  #       config = function()
-  #         -- require("java-deps").setup()
-  #       end,
-  #     },
-  #   }
-  # '';
+    plugins.spring-boot = ''
+      return {
+        {
+          "JavaHello/spring-boot.nvim",
+          ft = { "java", "yaml", "jproperties" },
+          dependencies = {
+            "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+          },
+          ---@type bootls.Config
+          opts = function()
+            return {
+              ls_path = require("spring_boot").get_boot_ls("${vscode-spring-boot-path}/language-server"),
+            }
+          end,
+        },
+      }
+    '';
+  };
 }
